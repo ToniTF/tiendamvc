69e103ad067527a407dcc27e0b88c13b524c289d
Tonitf <subcero00@hotmail.com>
2025-02-26 20:13:17 +0100

    product_ajax

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,27 +1,40 @@
 fetch("http://localhost/tiendamvc/api/categories")
-.then(data=>data.json())
-.then(datos=>{
-    datos.forEach(element => {
-        let option = document.createElement("option");
-        option.value = element.category_id;
-        option.textContent = element.name;
-        document.getElementById("category").appendChild(option);
-        
-    });
+    .then(data => data.json())
+    .then(datos => {
+        datos.forEach(element => {
+            let option = document.createElement("option");
+            option.value = element.category_id;
+            option.textContent = element.name;
+            document.getElementById("category").appendChild(option);
 
-})
-.catch(error=>console.log(error));
+        });
+
+    })
+    .catch(error => console.log(error));
 
 fetch("http://localhost/tiendamvc/api/providers")
-.then(data=>data.json())
-.then(datos=>{
-    datos.forEach(element => {
-        let option = document.createElement("option");
-        option.value = element.provider_id;
-        option.textContent = element.name;
-        document.getElementById("provider").appendChild(option);
-        
-    });
+    .then(data => data.json())
+    .then(datos => {
+        datos.forEach(element => {
+            let option = document.createElement("option");
+            option.value = element.provider_id;
+            option.textContent = element.name;
+            document.getElementById("provider").appendChild(option);
 
-})
-.catch(error=>console.log(error));
+        });
+
+    })
+    .catch(error => console.log(error));
+
+    document.getElementById("form").onsubmit = function (e) {
+        e.preventDefault();
+        let product={
+            'name': document.getElementById("name").value,
+            'description':document.getElementById("description").value,
+            'category_id':document.getElementById("category").value,
+            'provider_id':document.getElementById("provider").value,
+            'stock':document.getElementById("stock").value,
+            'price':document.getElementById("price").value
+        }
+        console.log(product);
+    }
