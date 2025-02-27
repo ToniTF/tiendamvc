5943b0032bef667511f5d39da1019e820204e5af
Tonitf <subcero00@hotmail.com>
2025-02-27 17:04:26 +0100

    product

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1,30 +1,46 @@
 <?php
 namespace Formacom\controllers;
 use Formacom\Core\Controller;
-use Formacom\Models\Provider;
-use Formacom\Models\Address;
-use Formacom\Models\Phone;
 use Formacom\Models\Customer;
-use Formacom\Models\Product;
+use Formacom\Models\Address;
 use Formacom\Models\Category;
+use Formacom\Models\Provider;
+use Formacom\Models\Product;
 
-class ApiController extends Controller {
-    public function index(...$params) {
-       
+class ApiController extends Controller{
+    public function index(...$params){
+        
     }
+
     public function categories() {
-        $categories = Category::all();
+        $categories=Category::all();
         $json=json_encode($categories);
         header('Content-Type: application/json');
         echo $json;
         exit();
     }
     public function providers() {
-        $provider = Provider::all();
-        $json=json_encode($provider);
+        $providers=Provider::all();
+        $json=json_encode($providers);
         header('Content-Type: application/json');
         echo $json;
         exit();
     }
+    public function newproduct(){
+        $data=json_decode(file_get_contents('php://input'),true);
+        $product=new Product();
+        $product->name=$data['name'];
+        $product->description=$data['description'];
+        $product->category_id=$data['category_id'];
+        $product->provider_id=$data['provider_id'];
+        $product->stock=$data['stock'];
+        $product->price=$data['price'];
+        $product->save();
+        $json=json_encode($product);
+        header('Content-Type: application/json');
+        echo $json;
+        exit();
+    }
+   
 }
 ?>
--- assets/js/product.js
+++ assets/js/product.js
@@ -6,11 +6,11 @@ fetch("http://localhost/tiendamvc/api/categories")
             option.value = element.category_id;
             option.textContent = element.name;
             document.getElementById("category").appendChild(option);
-
         });
-
     })
-    .catch(error => console.log(error));
+    .catch(err => {
+        console.log(err);
+    })
 
 fetch("http://localhost/tiendamvc/api/providers")
     .then(data => data.json())
@@ -20,21 +20,36 @@ fetch("http://localhost/tiendamvc/api/providers")
             option.value = element.provider_id;
             option.textContent = element.name;
             document.getElementById("provider").appendChild(option);
-
         });
-
     })
-    .catch(error => console.log(error));
+    .catch(err => {
+        console.log(err);
+    })
 
-    document.getElementById("form").onsubmit = function (e) {
-        e.preventDefault();
-        let product={
-            'name': document.getElementById("name").value,
-            'description':document.getElementById("description").value,
-            'category_id':document.getElementById("category").value,
-            'provider_id':document.getElementById("provider").value,
-            'stock':document.getElementById("stock").value,
-            'price':document.getElementById("price").value
-        }
-        console.log(product);
+document.getElementById("form").onsubmit = function (e) {
+    e.preventDefault();
+    let product={
+        'name': document.getElementById("name").value,
+        'description':document.getElementById("description").value,
+        'category_id':document.getElementById("category").value,
+        'provider_id':document.getElementById("provider").value,
+        'stock':document.getElementById("stock").value,
+        'price':document.getElementById("price").value
     }
+    //ToDO: Validar los datos
+    fetch("http://localhost/tiendamvc/api/newproduct",{
+        method: 'POST',
+        body: JSON.stringify(product),
+        headers:{
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(data => data.json())
+    .then(datos => {
+        if(datos.status==200){
+            alert("Producto registrado correctamente");
+        }else{
+            alert("Error al registrar el producto");
+        }
+    })
+}
